3c7b70239cdb59471487ac72cff3a771b5c23752
KushagraSharma10 <sharmakartik3888@gmail.com>
2025-04-12 00:01:31 +0530

    components folder and stylesheets folder is added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../stylesheets/Navbar.css"
+
+const Navbar = () => {
+  return (
+    <div className='nav'>
+        <h1 className='heading'>xinder</h1>
+        <div className='menu'><img src="src\assets\menu.png" alt="" /></div>
+    </div>
+  )
+}
+
+export default Navbar
