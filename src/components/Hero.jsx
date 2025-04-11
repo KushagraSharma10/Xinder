8c9c427abb97cceb1fd0714f4c43f029f2ffb9c1
KushagraSharma10 <sharmakartik3888@gmail.com>
2025-04-12 01:39:37 +0530

    hero componet is in working of mobile view

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,16 @@ import "../stylesheets/Hero.css"
 const Hero = () => {
   return (
     <div className='content'>
-        
+        <div className='text-content'>
+            <p>Search the work of best artists</p>
+            <div className='input-search'>
+                <input type="text" name="name" id="artists" />
+                <button>Search</button>
+            </div>
+        </div>
+        <div className='pic'>
+            <img src="src\assets\hero.png" alt="" />
+        </div>
     </div>
   )
 }
