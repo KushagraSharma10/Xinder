745a3322ffd757e0e5962b6ed642d00552cb105d
KushagraSharma10 <sharmakartik3888@gmail.com>
2025-04-12 00:52:42 +0530

    Navbar is completed for desktop

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
 import "../stylesheets/Navbar.css"
 
+
+
 const Navbar = () => {
+
+  const links = ["explore", "solutions", "articles", "sign in"]
+
   return (
     <div className='nav'>
         <h1 className='heading'>xinder</h1>
         <div className='menu'><img src="src\assets\menu.png" alt="" /></div>
+        {/* <div className='links'>
+          <a href="">explore</a>
+          <a href=""></a>
+          <a href=""></a>
+          <a href=""></a>
+        </div> */}
+        <div className='links'> 
+        {links.map((link , index) => {
+          return <a key={index} href="">{link}</a>
+        })}
+        <button>join now</button>
+        </div>
     </div>
   )
 }
