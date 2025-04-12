cd9e1593ec6b9e04bdbbb5d52c7ad5d9ef2ec3ae
KushagraSharma10 <sharmakartik3888@gmail.com>
2025-04-12 22:25:59 +0530

    Masterpiece .jsx and .css file is created and initialized

---
+++ src/components/Masterpiece.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "../stylesheets/Masterpiece.css"
+
+const Masterpiece = () => {
+  return (
+    <div className='latest'>
+        <div className='latest-headings'>
+                <h1>\Latest Masterpieces</h1>
+                <div className='discover'>
+                    <a href="">Discover more Masterpieces</a>
+                    <img src="src\assets\arrow-right.png" alt="" />
+                </div>
+            </div>
+    </div>
+  )
+}
+
+export default Masterpiece
