908bc5617e5f07ae2355c33c3ff3e3a66ce07271
KushagraSharma10 <sharmakartik3888@gmail.com>
2025-04-12 18:25:57 +0530

    cards portion is completed for mobile screen

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -1,33 +1,36 @@
 import React from 'react'
+import "../stylesheets/Featured.css"
 
 const Featured = () => {
 
     const cards = [
-        {pic: "src/assets/pic1.png", category: "pottery", heading: "Vases Family"},
-        {pic: "src/assets/pic2.png", category : "Oil Panting", heading: "Camel Portrait"},
-        {pic: "src/assets/pic3.png", category : "Abstract Panting", heading: "Euforia"},
-        {pic: "src/assets/pic4.png", category : "Object Design", heading: "Classic Watch"},
-        {pic: "src/assets/pic5.png", category : "Sculpture", heading: "White Angel"},
+        { pic: "src/assets/pic1.png", category: "pottery", heading: "Vases Family" },
+        { pic: "src/assets/pic2.png", category: "Oil Panting", heading: "Camel Portrait" },
+        { pic: "src/assets/pic3.png", category: "Abstract Panting", heading: "Euforia" },
+        { pic: "src/assets/pic4.png", category: "Object Design", heading: "Classic Watch" },
+        { pic: "src/assets/pic5.png", category: "Sculpture", heading: "White Angel" },
     ]
 
     return (
         <div className='features'>
-            <div>
+            <div className='text-headings'>
                 <h1>Featured art categories</h1>
-                <a href="">View All Categories</a>
+                <div className='view'>
+                    <a href="">View All Categories</a>
+                    <img src="src\assets\arrow-right.png" alt="" />
+                </div>
             </div>
-            <div>
-                
-                   {cards.map((card) => {
-                        return  <div>
-                            <img src={card.pic} alt="" />
-                            <div className='text'>
-                                <h3>{card.category}</h3>
-                                <h2>{card.heading}</h2>
-                            </div>
+            <div className='cards'>
+                {cards.map((card, index) => {
+                    return <div key={index} className='card'>
+                        <img src={card.pic} alt="" />
+                        <div className='text'>
+                            <h3>{card.category}</h3>
+                            <h2>{card.heading}</h2>
                         </div>
-                   })} 
-               
+                    </div>
+                })}
+
             </div>
         </div>
     )
