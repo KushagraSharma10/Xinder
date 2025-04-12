1e865d8d64dce7025f63cce819205906993272c2
KushagraSharma10 <sharmakartik3888@gmail.com>
2025-04-12 15:37:03 +0530

    incons are added ui for mobile for companies.jsx is completed

--- src/components/Companies.jsx
+++ src/components/Companies.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import "../stylesheets/Companies.css"
+
 
 const Companies = () => {
-  return (
-    <div>Companies</div>
-  )
+
+    const company_paths = ["src/assets/smart-finder.png", "src/assets/Zoomerr.png", "src/assets/Shells.png", "src/assets/Waves.png", "src/assets/artVenue.png"]
+
+    return (
+        <div className='companies'>
+            {company_paths.map((url, index) => {
+                return <div key={index} className='icons'>
+                    <img src={url} alt="" />
+                </div>
+            })}
+        </div>
+    )
 }
 
 export default Companies
