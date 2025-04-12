0000d8cf308586e23fb5d759d5d5513fb3a84bcd
KushagraSharma10 <sharmakartik3888@gmail.com>
2025-04-12 11:25:43 +0530

    search input with headings and artists divs are added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,28 @@
 import React from 'react'
 import "../stylesheets/Hero.css"
 const Hero = () => {
+
+  const arts = ["Oil painting", "Abstract Art", "Sculpture", "art deco", "pop art"]
+
   return (
     <div className='content'>
-        <div className='text-content'>
-            <p>Search the work of best artists</p>
-            <div className='input-search'>
-                <input type="text" name="name" id="artists" />
-                <button>Search</button>
-            </div>
+      <div className='text-content'>
+        <p>Search the work of best artists</p>
+        <div className='search-info'>
+          <div className='input-search'>
+            <input type="text" name="name" id="artists" placeholder='Search for artists, work name, or category' />
+            <button>Search</button>
+          </div>
+          <div className='arts'>
+            {arts.map((art, index) => {
+              return <div key={index} className='art'>{art}</div>
+            })}
+          </div>
         </div>
-        <div className='pic'>
-            <img src="src\assets\hero.png" alt="" />
-        </div>
+      </div>
+      <div className='pic'>
+        <img src="src\assets\hero.png" alt="" />
+      </div>
     </div>
   )
 }
