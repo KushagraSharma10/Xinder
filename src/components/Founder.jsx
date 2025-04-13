7ea861ee3b00e13792bf95a885744a23fdab2200
KushagraSharma10 <sharmakartik3888@gmail.com>
2025-04-13 17:33:13 +0530

    founder section is updated for laptop screens and some images are added

--- src/components/Founder.jsx
+++ src/components/Founder.jsx
@@ -2,18 +2,20 @@ import React from 'react'
 import "../stylesheets/Founder.css"
 
 const Founder = () => {
-  return (
-    <div className='founder'>
-        <div className='img-div'>
-            <img id='video' src="src\assets\video.png" alt="" />
-            <img id='playbtn' src="src\assets\play.png" alt="" />
+    return (
+        <div className='founder'>
+            <div className='inside'>
+                <div className='img-div'>
+                    <img id='video' src="src\assets\video.png" alt="" />
+                    <img id='playbtn' src="src\assets\play.png" alt="" />
+                </div>
+                <div className='text-div'>
+                    <h1>Marie Poirot, Co-Founder <span>Art-Gal</span></h1>
+                    <p>“Id urna, nisl, ut quam. Diam suspendisse fringilla quam arcu mattis est velit in. Nibh in purus sit convallis phasellus ut. Neque suspendisse a habitasse commodo.”</p>
+                </div>
+            </div>
         </div>
-        <div className='text-div'>
-            <h1>Marie Poirot, Co-Founder <span>Art-Gal</span></h1>
-            <p>“Id urna, nisl, ut quam. Diam suspendisse fringilla quam arcu mattis est velit in. Nibh in purus sit convallis phasellus ut. Neque suspendisse a habitasse commodo.”</p>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Founder
