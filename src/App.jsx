86ba414522e7255dc49a8ddcfa646c93b3a67fbb
KushagraSharma10 <sharmakartik3888@gmail.com>
2025-04-13 16:52:54 +0530

    founder section is completed for mobile screen

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Featured from './components/Featured'
 import Masterpiece from './components/Masterpiece'
 import Services from './components/Services'
 import Explore from './components/Explore'
+import Founder from './components/Founder'
 
 const App = () => {
   return (
@@ -18,6 +19,7 @@ const App = () => {
       <Masterpiece />
       <Services />
       <Explore />
+      <Founder />
     </div>
   )
 }
